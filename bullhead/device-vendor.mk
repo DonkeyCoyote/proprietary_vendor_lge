1ac1594ff7a383c604692c17c053a1421a472635
Jake Whatley <jacob.whatley91@gmail.com>
2016-02-01 14:11:54 -0500

    Revert "bullhead: bring inline with updated extract-files script"
    This reverts commit 38a35069ee677bda6796c83d44103af39bb2bf2e.

--- bullhead/device-vendor.mk
+++ bullhead/device-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ PRODUCT_PACKAGES += \
     rcsimssettings \
     rcsservice
 
-# Prebuilt privileged APKs
+# Prebuilt Privileged APKs
 PRODUCT_PACKAGES += \
     CNEService \
     ConnMO \
--- bullhead/proprietary/app/Android.mk
+++ bullhead/proprietary/app/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,18 +37,18 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := HiddenMenu
+LOCAL_MODULE := ims
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := HiddenMenu/HiddenMenu.apk
+LOCAL_SRC_FILES := ims/ims.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ims
+LOCAL_MODULE := HiddenMenu
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := ims/ims.apk
+LOCAL_SRC_FILES := HiddenMenu/HiddenMenu.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
